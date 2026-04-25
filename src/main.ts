db6f4ba2da4c516ac3e7cc5b3b2f70a719e1bb35
Diego Lima <xxdiegolsxx@gmail.com>
2026-04-25 11:23:56 -0300

    feat: :sparkles: configs
    main_ts

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,28 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
+import { ValidationPipe } from '@nestjs/common/pipes/validation.pipe';
 
 async function bootstrap() {
+  const logger = new Logger('Bootstrap');
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
+  const port = 3000;
+  await app.listen(port, '0.0.0.0');
+
+  const url = await app.getUrl();
+
+  logger.log(`🚀 Application is running on: ${url}`);
+  logger.log(
+    `📊 GraphQL Playground (Apollo Sandbox) available at: ${url}/graphql`,
+  );
 }
 bootstrap();
